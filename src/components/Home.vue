6c40774fb97a05b4af18c23a37c6ae727ede6595
Tom <hty1251817231@gmail.com>
2021-02-19 21:26:28 +0800

    das

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,151 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-buttom" @click="toggleCollapse">|||</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="true"
+          :default-active="activePath"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
+            <!-- 菜单模板 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconObj[item.id]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      menulist: [],
+      iconObj: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      isCollapse: false,
+      activePath: ''
+    }
+  },
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
   },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      //   console.log(res.data)
+      this.menulist = res.data
+    },
+
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-menu {
+  border-right: none;
+}
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
+
+.toggle-buttom {
+  background-color: greenyellow;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="../assets/abc.png" alt="" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+img {
+  width: 88%;
+}
+</style>
